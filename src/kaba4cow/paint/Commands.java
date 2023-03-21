25cf5685f4e9eb19b2084df371c9e60cb9989051
kaba4cow <kaba4cow@gmail.com>
2023-03-21 17:22:47 +0200

    update

--- src/kaba4cow/paint/Commands.java
+++ src/kaba4cow/paint/Commands.java
@@ -2,6 +2,7 @@ package kaba4cow.paint;
 
 import java.io.File;
 
+import kaba4cow.ascii.core.Display;
 import kaba4cow.ascii.toolbox.files.ImageFile;
 import kaba4cow.console.Command;
 import kaba4cow.console.Console;
@@ -77,6 +78,19 @@ public final class Commands {
 					output.append("File saved\n");
 				else
 					output.append("Could not save the file\n");
+			}
+		};
+
+		new Command("file-save-png", "", "Saves current file as a PNG image") {
+			@Override
+			public void execute(String[] parameters, int numParameters, StringBuilder output) {
+				if (AsciiPaint.project == null)
+					output.append("No file selected\n");
+				else if (Display.saveImage(AsciiPaint.project.toFrame(),
+						new File(Console.getDirectory().getAbsolutePath() + "/" + AsciiPaint.projectName + ".png")))
+					output.append("Image saved\n");
+				else
+					output.append("Could not save the image\n");
 			}
 		};
 
